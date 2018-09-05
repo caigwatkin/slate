d32eed2a558178e5cb0aee0e3b40973c84d7a038
Cai Gwatkin <caigwatkin@gmail.com>
2018-09-05 20:14:09 +1200

    add internal app and pkg scaffolds

--- go.mod
+++ go.mod
@@ -1,1 +1,9 @@
-module github.com/caigwatkin/slate
+module slate
+
+require (
+	github.com/caigwatkin/go v0.0.0-20180709185140-11ce7146b07a
+	github.com/google/uuid v1.0.0
+	github.com/pkg/errors v0.8.0
+	golang.org/x/oauth2 v0.0.0-20180620175406-ef147856a6dd
+	google.golang.org/api v0.0.0-20180621000839-3639d6d93f37
+)
---
+++ internal/pkg/errors/errors.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2018 Cai Gwatkin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package errors
+
+import (
+	"github.com/pkg/errors"
+)
+
+// New error with stack trace
+func New(message string) error {
+	return errors.New(message)
+}
+
+// Errorf formatted error with stack trace
+func Errorf(format string, args ...interface{}) error {
+	return errors.Errorf(format, args...)
+}
+
+// Wrap errors with a message
+func Wrap(err error, message string) error {
+	if err == nil {
+		return nil
+	}
+	if _, ok := err.(*Status); ok {
+		return err
+	}
+	return errors.Wrap(err, message)
+}
+
+// Wrapf errors with a formatted message
+func Wrapf(err error, format string, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+	if _, ok := err.(*Status); ok {
+		return err
+	}
+	return errors.Wrapf(err, format, args...)
+}
